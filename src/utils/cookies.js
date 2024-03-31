ea18dc7f280c3c60341884ccda08de5cc2c6439a
Roofi Ahmad Sidiq <roofiahmadsidiq@Roofis-MacBook-Air.local>
2024-03-31 15:11:22 +0700

    add error prevention

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,8 +1,10 @@
 export function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  if (document) {
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
 }
 
 export function getCookie(cname) {
