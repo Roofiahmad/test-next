754685cffb040b8b98b4e5c19e59729af53ebcad
Roofi Ahmad Sidiq <roofiahmadsidiq@Roofis-MacBook-Air.local>
2024-03-31 15:15:54 +0700

    add window checking

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,5 +1,5 @@
 export function setCookie(cname, cvalue, exdays) {
-  if (document) {
+  if (typeof window !== "undefined") {
     const d = new Date();
     d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
     let expires = "expires=" + d.toUTCString();
@@ -8,17 +8,19 @@ export function setCookie(cname, cvalue, exdays) {
 }
 
 export function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+  if (typeof window !== "undefined") {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == " ") {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+    return "";
   }
-  return "";
 }
